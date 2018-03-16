a1accc34a439479df54146b6eba5ca3517694313
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-03-16 08:31:13 +0300

    Login page user controller

--- ajandamWeb/Login.aspx.cs
+++ ajandamWeb/Login.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Newtonsoft.Json;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -19,5 +22,28 @@ public partial class Login : System.Web.UI.Page
         string email = String.Format("{0}", Request.Form["email"]);
         string password = String.Format("{0}", Request.Form["password"]);
 
+        string Url = "http://spring-kou-service.herokuapp.com/api/login";
+
+        string myParameters = $@"username={email}&password={password}";
+
+        
+
+        using (WebClient wc = new WebClient())
+        {
+            wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            string HtmlResult = wc.UploadString(Url, myParameters);
+            var JSON = JsonConvert.DeserializeObject(HtmlResult);
+
+            if (HtmlResult == "{}")
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                Response.Redirect("Admin.aspx");
+            }
+        }
+        
+       
     }
 }
