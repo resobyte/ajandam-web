dbe625f31e6a33fd436c1158a2fda7912f5e6742
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-03-18 01:13:36 +0300

    announcements add

--- ajandamWeb/Admin.aspx.cs
+++ ajandamWeb/Admin.aspx.cs
@@ -15,12 +15,29 @@ public partial class Admin : System.Web.UI.Page
     List<string> lessonClock = new List<string>();
     List<string> lessonDay = new List<string>();
     List<string> lessonLocation = new List<string>();
+
+    List<string> announcementsTitle = new List<string>();
+    List<string> announcementsContent = new List<string>();
+    List<string> announcementsDate = new List<string>();
+    List<string> announcementsLesson = new List<string>();
+
+
+    string rssID;
     string lessonsDiv = @"<table class='table'><thead class='blue-grey lighten-4'>
         <tr>
             <th>Ders Adı:</th>
             <th>Ders Günü:</th>
             <th>Ders Saati:</th>
             <th>Ders Yeri:</th>
+        </tr>
+    </thead><tbody>";
+
+    string announcementsDiv = @"<table class='table'><thead class='blue-grey lighten-4'>
+        <tr>
+            <th>Duyuru Başlık:</th>
+            <th>Duyuru İçerik:</th>
+            <th>Duyuru Tarihi:</th>
+            <th>Duyuru Dersi:</th>
         </tr>
     </thead><tbody>";
     protected void Page_Load(object sender, EventArgs e)
@@ -39,12 +56,12 @@ public partial class Admin : System.Web.UI.Page
         }
         else
         {
-            getData();
-
+            getLesson();
+            getAnnouncements();
         }
     }
 
-    public void getData()
+    public void getLesson()
     {
         string Url = "http://spring-kou-service.herokuapp.com/api/login";
 
@@ -56,6 +73,7 @@ public partial class Admin : System.Web.UI.Page
             wc.Encoding = System.Text.Encoding.UTF8;
             string HtmlResult = wc.UploadString(Url, myParameters);
             JObject rss = JObject.Parse(HtmlResult);
+            rssID = (string)rss["data"][0]["id"];
             string rssName = (string)rss["data"][0]["name"];
             string rssSurname = (string)rss["data"][0]["surname"];
 
@@ -73,6 +91,7 @@ public partial class Admin : System.Web.UI.Page
             }
             lessonsDiv += "</tbody></table>";
             username.InnerHtml = rssName + " " + rssSurname;
+            lessons.InnerHtml = "";
             lessons.InnerHtml = lessonsDiv;
         }
 
@@ -84,5 +103,33 @@ public partial class Admin : System.Web.UI.Page
         Response.Redirect("Login.aspx");
     }
 
+    public void getAnnouncements()
+    {
+        string Url = $"http://spring-kou-service.herokuapp.com/api/announcement/academician?academicianId={rssID}";
+        string myParameters =rssID;
+        using (WebClient wc = new WebClient())
+        {
+            wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            wc.Encoding = System.Text.Encoding.UTF8;
+            string HtmlResult = wc.DownloadString(Url);
+            JObject rss = JObject.Parse(HtmlResult);
+          
+            foreach (var announcements in rss["duyurular"])
+            {
+                announcementsTitle.Add((string)announcements["title"]);
+                announcementsContent.Add((string)announcements["content"]);
+                announcementsDate.Add((string)announcements["date"]);
+                announcementsLesson.Add((string)announcements["lesson"]["name"]);
+            }
+
+            for (int i = 0; i < announcementsTitle.Count; i++)
+            {
+                announcementsDiv += $"<tr><td>{announcementsTitle[i]}</td><td>{announcementsContent[i]}</td><td>{announcementsDate[i]}</td><td>{announcementsLesson[i]}</td></tr>";
+            }
+            announcementsDiv += "</tbody></table>";
+            lessons.InnerHtml = "";
+            lessons.InnerHtml = announcementsDiv;
+        }
+    }
 
 }
